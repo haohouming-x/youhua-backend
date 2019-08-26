80cf29b733af414bc01a417221b114b0e8897511
rip <371662584@qq.com>
2019-08-26 15:32:59 +0800

    fixed bug for float

--- src/Admin/OrderAdmin.php
+++ src/Admin/OrderAdmin.php
@@ -244,7 +244,7 @@ final class OrderAdmin extends AbstractAdmin
         if($order->getTotalProfit() >= 0) return;
 
         $this->pay_refund
-            ->tradeRefund($order->getOrderNumber(), $order->getTotal(), abs($order->getTotalProfit()), [
+            ->tradeRefund($order->getOrderNumber(), $order->getTotal(), round(abs($order->getTotalProfit()), 2), [
                 'refund_desc' => '已归还油画'
             ]);
     }
